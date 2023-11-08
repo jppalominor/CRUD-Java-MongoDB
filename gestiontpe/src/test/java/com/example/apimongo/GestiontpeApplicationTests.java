9e78361674e9ae7a6c2fa2dd2d3c8cac1b193d01
jppalominor <j.palominor@hotmail.com>
2023-11-07 19:10:42 -0500

    carga de datos, creacion proyecto en java

---
+++ gestiontpe/src/test/java/com/example/apimongo/GestiontpeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apimongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestiontpeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

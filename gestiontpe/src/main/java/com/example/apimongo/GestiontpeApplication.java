e30891d450b03789f939ddcfd110bb4299c43cbf
jppalominor <j.palominor@hotmail.com>
2023-11-10 17:08:39 -0500

    configuracion conexión a bd

--- gestiontpe/src/main/java/com/example/apimongo/GestiontpeApplication.java
+++ gestiontpe/src/main/java/com/example/apimongo/GestiontpeApplication.java
@@ -1,10 +1,15 @@
 package com.example.apimongo;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class GestiontpeApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(GestiontpeApplication.class, args);
